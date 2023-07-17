7477c2908d7e167bd6b6e41324415a95e693ca78
bfreeman12 <bfree98@icloud.com>
2023-07-17 15:48:42 -0400

    added styling and card for ui

--- src/App.js
+++ src/App.js
@@ -1,32 +1,70 @@
 import React, { useState, useEffect } from "react";
 import weatherFetch from "./functions/api-query";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import "./assets/searchbar.css";
+import "./assets/weathercard.css";
 
 function App() {
-
-  const [storedWeather, setStoredWeather] = useState()
-  const handleClick = async (e) => {
+  const [storedWeather, setStoredWeather] = useState();
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    const queryInput = document.getElementById("searchbar").value
+    const queryInput = document.getElementById("searchbar").value;
     await weatherFetch(queryInput).then((data) => {
-      setStoredWeather(data)
-    })
-  }
+      setStoredWeather(data);
+    });
+  };
 
   useEffect(() => {
     if (storedWeather) {
-      console.log(storedWeather)
+      console.log(storedWeather);
     }
-  }, [storedWeather])
+  }, [storedWeather]);
 
   return (
     <div className="app">
-      <div className="search-bar">
-        <input id="searchbar" placeholder="Please enter a city or zip code.." />
-        <button onClick={e => handleClick(e)}>Submit</button>
-      </div>
-      <div className="weather-content-container">
-        weather content goes here
-      </div>
+      <header className="page-header">
+        <div className="search-bar">
+          <form className="search-form" onSubmit={(e) => handleSubmit(e)}>
+            <input
+              className="search-input"
+              id="searchbar"
+              placeholder="Please enter a city or zip code.."
+              autoComplete="off"
+              autoCorrect="off"
+              required
+            />
+            <button className="search-button" formAction="submit">
+              <FontAwesomeIcon icon={faMagnifyingGlass} />
+            </button>
+          </form>
+        </div>
+      </header>
+      <body className="page-content">
+        {storedWeather ? (
+          <div className="weather-content-container">
+            <h5 className="city-name">{storedWeather.name}</h5>
+            <div className="weather-content-body">
+              <div className="city-temperature">
+                It is currently: {Math.floor(storedWeather.main.temp)}F
+              </div>
+
+              <div className="city-feels-like">
+                Feels like: {Math.floor(storedWeather.main.feels_like)}F
+              </div>
+
+              <div className="city-current-weather">
+                Current weather: {storedWeather.weather[0].main}
+              </div>
+              <div className="city-current-wind">
+                {Math.floor(storedWeather.wind.speed)} mph winds
+              </div>
+            </div>
+          </div>
+        ) : (
+          <div>No Data</div>
+        )}
+      </body>
     </div>
   );
 }
--- src/functions/api-query.js
+++ src/functions/api-query.js
@@ -1,16 +1,18 @@
-import axios from 'axios'
+import axios from "axios";
 
 const weatherFetch = async (queryInput) => {
-    try {
-        const response = await axios.post("http://10.0.0.248:8432/api/fetch-weather", {
-            queryInput
-        })
-        const data = response.data
-        return data.data
-    } catch (error) {
-        alert("Error please try again")
-        console.error("Error fetching weather", error)
-    }
-}
-export default weatherFetch
-
+  try {
+    const response = await axios.post(
+      "http://192.168.86.48:8432/api/fetch-weather",
+      {
+        queryInput,
+      }
+    );
+    const data = response.data;
+    return data.data;
+  } catch (error) {
+    alert("Error please try again");
+    console.error("Error fetching weather", error);
+  }
+};
+export default weatherFetch;
