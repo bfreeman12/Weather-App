307cacbc9685f6acdd19ccfa6f049311c4b00ed3
Brandon <bfree98@icloud.com>
2023-07-18 00:06:14 -0400

    added more styling

--- src/App.js
+++ src/App.js
@@ -4,9 +4,13 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 import "./assets/searchbar.css";
 import "./assets/weathercard.css";
+import bonoksphere from "./assets/bonkosphere.png"
 
 function App() {
   const [storedWeather, setStoredWeather] = useState();
+  const [weatherIcon, setWeatherIcon] = useState("");
+  const [weatherIconUrl, setWeatherIconUrl] = useState("");
+
   const handleSubmit = async (e) => {
     e.preventDefault();
     const queryInput = document.getElementById("searchbar").value;
@@ -17,14 +21,17 @@ function App() {
 
   useEffect(() => {
     if (storedWeather) {
-      console.log(storedWeather);
+      setWeatherIcon(storedWeather.weather[0].icon);
+      const iconUrl = `http://openweathermap.org/img/wn/${weatherIcon}.png`;
+      setWeatherIconUrl(iconUrl);
     }
-  }, [storedWeather]);
+  }, [storedWeather, weatherIcon]);
 
   return (
     <div className="app">
       <header className="page-header">
         <div className="search-bar">
+          <img id="bonoksphere" height={200} src={bonoksphere} alt="bonkosphere" />
           <form className="search-form" onSubmit={(e) => handleSubmit(e)}>
             <input
               className="search-input"
@@ -42,30 +49,36 @@ function App() {
       </header>
       <body className="page-content">
         {storedWeather ? (
-          <div className="weather-content-container">
-            <h5 className="city-name">{storedWeather.name}</h5>
-            <div className="weather-content-body">
-              <div className="city-temperature">
-                It is currently: {Math.floor(storedWeather.main.temp)}F
-              </div>
+          <div className="weather-sticky-notes">
+            <div className="weather-content-container">
+              <img id="wicon" src={weatherIconUrl} alt="Weather icon" />
+              <h5 className="city-name">{storedWeather.name}</h5>
+              <div className="weather-content-body">
+                <div className="city-temperature">
+                  It is currently: {Math.floor(storedWeather.main.temp)}F
+                </div>
 
-              <div className="city-feels-like">
-                Feels like: {Math.floor(storedWeather.main.feels_like)}F
-              </div>
+                <div className="city-feels-like">
+                  Feels like: {Math.floor(storedWeather.main.feels_like)}F
+                </div>
 
-              <div className="city-current-weather">
-                Current weather: {storedWeather.weather[0].main}
-              </div>
-              <div className="city-current-wind">
-                {Math.floor(storedWeather.wind.speed)} mph winds
+                <div className="city-current-weather">
+                  Current weather: {storedWeather.weather[0].main}
+                </div>
+                <div className="city-current-wind">
+                  {Math.floor(storedWeather.wind.speed)} mph winds
+                </div>
               </div>
             </div>
           </div>
+
         ) : (
-          <div>No Data</div>
-        )}
-      </body>
-    </div>
+          <div className="no-data-message">No Data</div>
+        )
+        }
+
+      </body >
+    </div >
   );
 }
 
